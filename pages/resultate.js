f5306e8d9bd241333621d3ed13bd7e36105a0e8b
Marcel Weber <mrweber@gmx.ch>
2022-11-16 13:41:27 +0100

    Working dropbox file link getter

--- pages/resultate.js
+++ pages/resultate.js
@@ -1,9 +1,37 @@
-export default function Resultate(data){
-    
-   console.log(data.data.entries)
+import Link from "next/link"
+import s from "../styles/Resultate.module.css"
+
+export default function Resultate({getFolders, getFiles, getPaths, getLinks}){
 
     return(
-        <div>Hello</div>
+        <>
+        <h1>Resultate</h1>
+        {
+            getFolders.map(folder => {
+                return (
+                    <>
+                    <strong>{folder.name}<br/></strong>
+                    <div className={s.resultContainer}>
+                    {getFiles.map(file=>{
+                        if(file.path_display.substring(0, file.path_display.lastIndexOf("/")) == folder.path_display){
+                          return getLinks.map(link =>{
+                            if(file.name == link.metadata.name){
+                                return <Link href={link.link} className={s.result}>{file.name}</Link>
+                            }
+                        })
+                           
+                               // return <Link href={str}>{file.name}</Link>
+                            
+                            
+                        } 
+                    })}
+                    </div>
+                    </>
+                )
+            })
+        }
+        
+        </>
     )
 }
 
@@ -14,9 +42,42 @@ export async function getServerSideProps() {
             Authorization : `Bearer ${process.env.DROPBOX}`,
             'Content-Type' : "application/json; charset=utf-8"
         },
-        body: JSON.stringify({path: "", recursive: true})
-    }).then(res => res.json())
- 
-    return { props: { data } }
+        body: JSON.stringify({path: "/Resultate", recursive: true})
+    })
+    
+    const res = await data.json()
+
+    const getFolders = res.entries.filter((item, index)=>{
+        if(item[".tag"] == "folder"){
+            return item
+        }
+    })
+
+    const getFiles = res.entries.filter((item, index) =>{
+        if(item[".tag"] == "file"){
+            return item
+        }
+    })
+
+    const getPaths = getFiles.map(file=>{
+        return file.path_display
+    })
+   
+    const getLinks = await Promise.all(getPaths.map(async (getPath) => {
+        const data = await fetch("https://api.dropboxapi.com/2/files/get_temporary_link", {
+            method : "POST",
+            headers: {
+                Authorization : `Bearer ${process.env.DROPBOX}`,
+                'Content-Type' : "application/json; charset=utf-8"
+            },
+            body: JSON.stringify({path: getPath})
+        })
+
+        const res = await data.json()
+        return res
+    }))
+
+    
+    return { props: { getFolders, getFiles, getPaths, getLinks } }
 }
 
