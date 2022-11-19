e2492b215d99a68a903096f7b19319d01e9d3b07
Marcel Weber <mrweber@gmx.ch>
2022-11-19 02:37:28 +0100

    Frontpage styling

--- pages/index.js
+++ pages/index.js
@@ -5,29 +5,27 @@ import s from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={s.container}>
+    <main className="main">
       <Head>
         <title>Pistolenclub Hallau</title>
         <meta name="description" content="Pistolenclub Hallau" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main className={s.main}>
+      <section className="section">
         <h1 className={s.title}>
           Pistolenclub Hallau
         </h1>
-           <Link href="/anlaesse">Anlässe</Link>
-           <Link href="/resultate">Resultate</Link>
-           <div className={s.logo}></div>
-           <Link href="/mitmachen">Mitmachen</Link>
-           <Link href="/jahresprogramm">Jahresprogramm</Link>
-           <Link href="/schiessen/schwabenkrieg-erinnerungsschiessen">Schwabenkrieg-Erinnerungsschiessen 2023</Link>
-
-        </main>
-
-      <footer className={s.footer}>
-       
+        <div className={s.linkContainer}>
+          <Link className={s.link} href="/anlaesse"><span className={s.text}>Anlässe</span></Link>
+          <Link className={s.link} href="/resultate"><span className={s.text}>Resultate</span></Link>
+          <div className={s.logo}></div>
+          <Link className={s.link} href="/mitmachen"><span className={s.text}>Mitmachen</span></Link>
+          <Link className={s.link} href="/jahresprogramm"><span className={s.text}>Jahres-programm</span></Link>
+        </div>
+        <Link className={s.skes} href="/schiessen/schwabenkrieg-erinnerungsschiessen"><span className={s.skesText}>Schwabenkrieg-Erinnerungsschiessen 2023</span></Link>     
+      </section>
+      <footer className="footer">
       </footer>
-    </div>
+    </main>
   )
 }
