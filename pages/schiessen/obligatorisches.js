d7b4bdc796b03a4b13aa4e16f9046efe67fa5dea
Marcel Weber <mrweber@gmx.ch>
2022-11-25 10:18:36 +0100

    Added Page for Obligatorisches

---
+++ pages/schiessen/obligatorisches.js
@@ -0,0 +1,10 @@
+export default function Obli(){
+    return (
+        <main className="main">
+            <section className="section">
+                <h1>Obligatorisches</h1>
+                
+            </section>
+        </main>
+    )
+}
