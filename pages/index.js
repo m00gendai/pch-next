16eb35264da5fd0da29b84361a85f064d0aa0897
Marcel Weber <mrweber@gmx.ch>
2022-11-25 10:21:44 +0100

    Removed unused Image import

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import Link from "next/link"
 import s from '../styles/Home.module.css'
 
 export default function Home() {
+
   return (
     <main className="main">
       <Head>
