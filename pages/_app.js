16f1494a5c8f8e0514dfde1e3ee89c39caa6bae6
Marcel Weber <mrweber@gmx.ch>
2022-11-25 10:17:25 +0100

    Conditional menubar rendering

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,30 @@
 import Navbar from "../components/navbar"
+import Navbar_Mobile from "../components/navbar_mobile"
 import '../styles/globals.css'
+import { useState } from "react"
 
 function MyApp({ Component, pageProps }) {
+
+  const [innerWidth, setInnerWidth] = useState(getWindowSize())
+
+  function getWindowSize() {
+    if(typeof window != "undefined"){
+      return window.innerWidth
+    }
+  }
+
+  if(typeof window != "undefined"){
+    window.addEventListener("resize", function(){
+      setInnerWidth(getWindowSize())
+    })
+  }
+
   return(
     <>
+    {innerWidth >= 765 ?
     <Navbar />
+    :
+    <Navbar_Mobile />}
     <Component {...pageProps} />
     </>
   ) 
