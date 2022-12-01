4542a0ea76e99a219ed5d43922004935758e7496
Marcel Weber <mrweber@gmx.ch>
2022-12-02 00:50:23 +0100

    i18n key moved

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
-
-module.exports = {
-  nextConfig,
   i18n: {
     locales: ["de"],
     defaultLocale: "de",
   }
+}
+
+module.exports = {
+  nextConfig  
 }
