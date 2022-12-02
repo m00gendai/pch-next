17c6c8f3549e86ef5eec89aba33afa052d73d05c
Marcel Weber <mrweber@gmx.ch>
2022-12-02 04:37:24 +0100

    Footer styles based on device

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,20 +1,37 @@
 import Link from "next/link"
 import s from "../styles/footer.module.css"
+import { useMediaQuery } from '@react-hook/media-query'
 
 export default function Footer(){
 
+    const isMobile = useMediaQuery('only screen and (orientation: portrait)')
     const date = new Date()
     const currentYear = date.getFullYear()
 
     return(
         <footer className={s.footer}>
-            <p>
-                ©{currentYear.toString() == "2022" ? "2022" : `2022 - ${currentYear}`} Pistolenclub Hallau
-            </p>
-            <div className={s.linkContainer}>
-                <Link className={s.link} href="/impressum">Impressum</Link>
-                <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
-            </div>
+            {
+                isMobile
+                ?
+                <>
+                    <p>
+                        ©{currentYear.toString() == "2022" ? "2022" : `2022 - ${currentYear}`} Pistolenclub Hallau
+                    </p>
+                    <div className={s.linkContainer}>
+                        <Link className={s.link} href="/impressum">Impressum</Link>
+                        <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
+                    </div>
+                </>
+                :
+                <>
+                    <Link className={s.link} href="/impressum">Impressum</Link>
+                    <p>
+                        ©{currentYear.toString() == "2022" ? "2022" : `2022 - ${currentYear}`} Pistolenclub Hallau
+                    </p>
+                    <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
+                </>
+            }
+            
         </footer>
     )
 }
