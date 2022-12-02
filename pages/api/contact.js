f05b76c1df02d7e64a2b3b2a5da2a6987f11e335
Marcel Weber <mrweber@gmx.ch>
2022-12-02 22:56:14 +0100

    Added API route to handle SMTP connetion for contact forms ALso renamed it

---
+++ pages/api/contact.js
@@ -0,0 +1,38 @@
+/*
+  Code shamelessly stolen, taken, mugged, thieved, looted, pilfered, appropriated, robbed, raided, burgled, embezzled and snatched 
+  from https://medium.com/nerd-for-tech/coding-a-contact-form-with-next-js-and-nodemailer-d3a8dc6cd645 
+*/
+
+export default function (req, res) {
+
+  let nodemailer = require('nodemailer')
+
+  const transporter = nodemailer.createTransport({
+    port: 465,     
+    host: "mail.cyon.ch",
+      auth: {
+        user: 'test@mrweber.ch',
+        pass: `${process.env.SMTP}`,
+      },
+    secure: true,
+  });
+    
+  const mailData = {
+      from: req.body.email,
+      to: 'test@mrweber.ch',
+      subject: `PCH: Nachricht von ${req.body.name}`,
+      text: req.body.message,
+      html: `<div>${req.body.message}</div>`
+  }
+  
+  transporter.sendMail(mailData, function (err, info) {
+    if(err)
+      console.log(err)
+    else
+      console.log(info);
+  })
+  
+  console.log(req.body)
+  res.send('success')
+
+  }
