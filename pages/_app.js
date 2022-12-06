d39d3c758f62af0d58f8c7963dc0640886ef32b0
Marcel Weber <mrweber@gmx.ch>
2022-12-06 17:29:24 +0100

    Changed mobile detection

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ import { useMediaQuery } from '@react-hook/media-query'
 
 function MyApp({ Component, pageProps }) {
 
-  const isMobile = useMediaQuery('only screen and (orientation: portrait)')
+  const isMobile = useMediaQuery('only screen and (max-aspect-ratio: 13/9)')
 
   return(
     <>
