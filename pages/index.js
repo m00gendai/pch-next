aa57dcaa4ad13c5e798ce9cb11c501694e9d1866
Marcel Weber <mrweber@gmx.ch>
2022-12-07 12:37:38 +0100

    Changed next/head to dedicated header component

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,17 @@
-import Head from 'next/head'
 import Link from "next/link"
 import s from '../styles/Home.module.css'
 import { useMediaQuery } from '@react-hook/media-query'
+import Header from "../components/header"
+import { useRouter } from 'next/router'
 
 export default function Home() {
-
+  const router = useRouter()
+  const headUrl = `https://pistolenclub-hallau.ch${router.pathname}`
   const isMobile = useMediaQuery('only screen and (orientation: portrait)')
 
   return (
     <main className="main">
-      <Head>
-        <title>Pistolenclub Hallau</title>
-        <meta name="description" content="Pistolenclub Hallau" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="Pistolenclub Hallau" />
-        <meta name="twitter:description" content="Der Pistolenclub in den Hallauer Weinbergern" />
-        <meta name="twitter:image" content="https://zingy-daffodil-ca4782.netlify.app/pch-logo.png" />
-        <meta property="og:type" content="article" />
-        <meta property="og:title" content="Pistolenclub Hallau" />
-        <meta property="og:description" content="Der Pistolenclub in den Hallauer Weinbergern" />
-        <meta property="og:url" content="https://zingy-daffodil-ca4782.netlify.app" />
-        <meta property="og:image" content="https://zingy-daffodil-ca4782.netlify.app/pch-logo.png" />
-        <link rel="icon" href="/pch-logo.png" />
-      </Head>
+      <Header title={"Pistolenclub Hallau"} content={"Der Pistolenclub in den Hallauer Weinbergen"} url={headUrl} />
       <section className="section">
         <h1 className={s.title}>
           Pistolenclub Hallau
