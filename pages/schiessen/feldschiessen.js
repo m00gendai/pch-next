a525cb1942fc1467d44c9a0d32971a3f9fb66aad
Marcel Weber <mrweber@gmx.ch>
2022-12-08 11:41:09 +0100

    Implemented current year conditional rendering

--- pages/schiessen/feldschiessen.js
+++ pages/schiessen/feldschiessen.js
@@ -2,15 +2,57 @@ import Link from "next/link"
 import Image from "next/image"
 import Header from "../../components/header"
 import { useRouter } from 'next/router'
+import { fsTimes } from "../../lib/fsInfo"
 import s from "../../styles/Feldschiessen.module.css"
 
-export default function FS(){
+export default function FS(
+    { 
+        sourceDirectoryList,
+        links
+    }
+){
 
     const router = useRouter()
     const headUrl = `https://pistolenclub-hallau.ch${router.pathname}`
 
     const date = new Date()
     const currentYear = date.getFullYear()
+
+    const results = sourceDirectoryList.data
+
+    const containsCurrentYear = results.filter(result =>{
+        return result.name == currentYear.toString() && result.type == "dir"
+    })
+
+    const currentYearDirs = results.filter(result =>{ // This filters for all directories of the current year
+        return result.name == currentYear.toString()
+    })
+
+    const currentYearDirIds = currentYearDirs.map(item=>{ // this extracts the parent_id of the current year directories
+        return item.parent_id
+    })
+
+    function getFile(id){
+        const url = links.data.filter(link=>{
+            return link.id == id        
+        })
+        window.open(url[0].temporary_url, "_blank")
+    }
+
+    function renderMap(place){
+        switch(place){
+            case "Neunkirch":
+                return (
+                    <iframe className={s.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5630.899465882173!2d8.493181576693809!3d47.67763207994218!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47907b4c174e6b41%3A0xb6068ef6947cbe55!2sPistolenstand%20Horn!5e1!3m2!1sen!2sch!4v1670495150749!5m2!1sen!2sch"></iframe>
+                )
+                break
+            case "Neuhausen":
+                return (
+                    <iframe className={s.map} src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2109.3356584343683!2d8.58849526125184!3d47.674377609921684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x479078d6ab0a6bbd%3A0xc60b5fb3f18dfd2!2sLangrietstrasse%20100%2C%208212%20Neuhausen%20am%20Rheinfall!5e1!3m2!1sen!2sch!4v1670495270414!5m2!1sen!2sch" ></iframe>
+                )
+                break
+        }
+    }
 
     return (
         <main className="main">
@@ -32,11 +74,48 @@ export default function FS(){
                     </p>
                 </div>
                 <h2>Resultate {currentYear}</h2>
+                {containsCurrentYear.length != 0 ?
+                    <div className={s.resultContainer}>
+                        {
+                            results.map(result=>{
+                                if(result.type == "dir" && result.name == currentYear.toString()){
+                                    return results.map(result2 =>{
+                                        if(result2.type == "file" && result2.parent_id == result.id){
+                                            const name = result2.name.replaceAll("_", " ").replace(".pdf", "")
+                                            return <div key={`FSresult${result2.id}`} className={s.link} onClick={()=>getFile(result2.id)}>{name}</div>
+                                        }
+                                    })
+                                } 
+                            }) 
+                        }
+                    </div>
+                    :
+                    <p>Das Feldschiessen fand dieses Jahr noch nicht statt oder die Resultate sind noch nicht verfügbar.</p>
+                }
                 <h2>Feldschiessen {currentYear}</h2>
                 <div className={s.container}>
-                    <p>
-                        Zeiten und Orte werden noch bekannt gegeben
-                    </p>
+                    {fsTimes.year == currentYear ? 
+                        <p>Gruppenplatz für den Kreis Unterklettgau ist dieses Jahr {fsTimes.name}.</p>
+                    :
+                        null
+                    }
+                    {fsTimes.year == currentYear ?
+                    <>
+                        <p>Schiesszeiten:</p>
+                        <p>
+                            {fsTimes.times.map(item=>{
+                                return <>{item.date} {item.time} <br /></>
+                            })}
+                        </p>
+                    </>
+                    :
+                        <p>Zeiten & Ort noch nicht bekannt</p>
+                    }
+                    {fsTimes.year == currentYear ?
+                        renderMap(fsTimes.name)
+                    :
+                    null
+                    }
                 </div>
                 <h2>Programm 50m</h2>
                 <div className={s.container}>
@@ -97,4 +176,50 @@ export default function FS(){
             </section>
         </main>
     )
+}
+
+export async function getStaticProps(){
+
+// Feldschiessen folder ID = 414
+
+// Gets all folders and files in the /Resultate directory recursively, sorted by last modified
+    const getSourceDirectoryList = await fetch("https://api.infomaniak.com/2/drive/608492/files/search?directory_id=414&depth=unlimited&per_page=1000&order_by=last_modified_at", {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${process.env.KDRIVE}`,
+            "Content-Type" : "application/json"
+        },
+
+    })
+    const sourceDirectoryList = await getSourceDirectoryList.json()
+
+    const files = sourceDirectoryList.data.filter(item=>{ // filters for files only
+        return item.type == "file"
+    })
+
+    const fileIds = files.map(file =>{ // gets the file ids
+        return file.id
+    })
+
+    // Gets temporary urls for all file ids
+    const getLinks = await fetch("https://api.infomaniak.com/2/drive/608492/files/temporary_urls", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${process.env.KDRIVE}`,
+            "Content-Type" : "application/json"
+        },
+        body: JSON.stringify(
+            {
+                "ids": fileIds
+            }
+        ),
+    })
+    const links = await getLinks.json()
+       
+    return { 
+        props: {
+            sourceDirectoryList,
+            links
+        }, revalidate: 2
+    }
 }
