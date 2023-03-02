161207485c9fc9b95a7c77f52879f1764c6f0fe5
Marcel Weber <mrweber@gmx.ch>
2023-03-02 16:40:15 +0100

    Amended with Spinner state toggle

--- functions/getFile.js
+++ functions/getFile.js
@@ -1,7 +1,8 @@
-export default function getFile(id){
+export default function getFile(id, setShow){
+   setShow == null ? null : setShow(true)
     const setFileId = async function(){
         let file = {"file_id" : id}
-        
+
         const response = await fetch('/api/download', {
         method: 'POST',
         headers: {
@@ -11,11 +12,11 @@ export default function getFile(id){
         body:
             JSON.stringify(file)
         })
-
         return await response.json()
     }
     
     setFileId().then((data) =>{
+        setShow == null ? null : setShow(false)
         window.open(data.url, "_blank")
     })
 }
