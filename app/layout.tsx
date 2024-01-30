4dc82d3b9117579a7494b613124b661035fb21ec
Marcel Weber <mrweber@gmx.ch>
2024-01-30 07:49:49 +0100

    Migrated _app to App Router

---
+++ app/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar"
+import Navbar_Mobile from "@/components/navbar_mobile"
+import Footer from "@/components/footer"
+
+export default function RootLayout({children}:{children:React.ReactNode}){
+    return(
+        <html lang="de">
+            <body>
+                <Navbar_Mobile />
+                <Navbar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    )
+}
