9cd87e26f7e9797dc7b6c4bd262eccf700b021c1
Marcel Weber <mrweber@gmx.ch>
2024-02-01 13:15:13 +0100

    Refactored for CMS delivery

--- app/anlaesse/page.tsx
+++ app/anlaesse/page.tsx
@@ -1,92 +1,51 @@
-import { FileResponse, ShootTime } from "@/interfaces"
-import s from "@/styles/Anlaesse.module.css" 
-import { convertDate } from "@/utils"
-import revalidate from "@/app/actions/revalidate"
+import { PageContent } from "@/interfaces"
+import { innerTextReplacer } from "@/utils"
 import React from "react"
-import LinkContainer from "@/components/LinkContainer"
+import DocumentContainer from "@/components/DocumentContainer"
+import TableContainer from "@/components/TableContainer"
 
-async function getShootTimes(){
-    const getShootTimes:Response = await fetch(
-        'https://cms.pistolenclub-hallau.ch/api/content/items/shootTimes?populate=100',
+async function getPageContent(){
+    const getPageContent:Response = await fetch(
+        'https://cms.pistolenclub-hallau.ch/api/content/items/anlaesse?populate=100',
         {
             headers: {
                 'api-key': `${process.env.CMS}`,
             },
             next: {
-                tags: [
-                    "shoot-times"
-                ]
+                revalidate: 10,
             }
         }
     )
 
-    const shootTimes:ShootTime[] = await getShootTimes.json()
-    return shootTimes
-}
-
-async function getFiles(){
-
-    const getFiles:Response = await fetch(
-        `https://api.infomaniak.com/2/drive/${process.env.DRIVE}/files/search?with=path&directory_id=${process.env.HEIMSTICHE}&depth=unlimited&types[]=file&per_page=1000`,
-        {
-            method: "GET",
-            headers: {
-                Authorization: `Bearer ${process.env.KDRIVE}`,
-                "Content-Type": "application/json",
-            },
-        }
-    );
-    
-    const files:FileResponse = await getFiles.json();
-    return files
+    const pageContent:PageContent[] = await getPageContent.json()
+    return pageContent
 }
 
 export default async function Anlaesse(){
-    revalidate("shoot-times")
-    const shootTimes:ShootTime[] = await getShootTimes()
-    const files:FileResponse = await getFiles()
+    const pageContent:PageContent[] = await getPageContent()
 
     return (
         <main className="main">
             <section className="section">
                 <h1>Anlässe</h1>
-                <h2>Kalender</h2>
-                    <iframe className={s.calendar} src="https://calendar.google.com/calendar/embed?height=600&wkst=2&bgcolor=%23ffffff&ctz=Europe%2FBrussels&showTitle=0&showNav=1&showCalendars=0&showTz=0&src=NmFsa2FmYjZnN2JqOWJnaWw0MzFjdmlzMjBAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ&color=%23F6BF26" width="800" height="600" ></iframe>
-                <h2>Besonderes</h2>
-                    <ul className={s.ul}>
-                        <li>
-                            Trainingszeiten: Jeweils Mittwochs bis zum Einnachten. Im Sommer ab 19:00, Frühling/Herbst ab 18:00, einsehbar im Kalender.
-                        </li>
-                    </ul>
-                <h2>Heimstiche</h2>
-                    <div className={s.container}>
-                        Heimstiche sind Wettberwerbe, die im heimischen Schiessstand geschossen werden können. Wir schiessen folgende Stiche:
-                            <ul className={s.ul}>
-                                {shootTimes.map(shoot =>{
-                                    return(
-                                        <li key={shoot._id} dangerouslySetInnerHTML={{
-                                            __html: `<strong>${shoot.name}:</strong> ${shoot.remarks === null ? `von ${convertDate(shoot.startDate as string)} bis und mit ${convertDate(shoot.endDate as string)}` : `${shoot.remarks}`} `
-                                        }}>
-                                        </li>
-                                    )
-                                })}
-                            </ul>
-                        <LinkContainer files={files} />
-                    </div>
-                <h2>Jahresmeisterschaft</h2>
-                    <div className={s.container}>
-                        Jedes Jahr küren wir den besten Hallauer Pistolenschützen. Die Bestresultate der folgenden Stiche 2022 zählen nun zur Jahreswertung:
-                        <ul>
-                            <li>Schwabenkrieg-Erinnerungsschiessen</li>
-                            <li>Feldschiessen (umgerechnet auf 25m wenn auf 50m geschossen)</li>
-                            <li>Bundesprogramm 50m</li>
-                            <li>Heimwettkampf</li>
-                            <li>Matchfonds – Jeweils höchste Passe auf die P- und B-Scheibe</li>
-                            <li>Fufzgerstich, höchste Passe (ehemals Goldstich)</li>
-                            <li>Endstich – Ist bei Verhinderung am Endschiessen vorzuschiessen</li>
-                        </ul>
-                        Die aktuelle Rangliste der Jahresmeisterschaft hängt im Schiessstand aus.
-                    </div>
+                {pageContent.map(content=>{
+                    return(
+                        <>
+                        <h2>{content.chapter.title}</h2>
+                        {content.chapter.content.map(elements =>{
+                            return(
+                                <div className="chapter">
+                                {elements.text ? <div className="chapter_text" dangerouslySetInnerHTML={{__html: innerTextReplacer(elements.text)}}></div> : null}
+                                {elements.table ? <TableContainer table={elements.table} /> : null}
+                                {elements.documents ? <DocumentContainer files={elements.documents} /> : null}
+                                </div>
+                            )
+                        }
+                            )}
+                        </>
+                    )
+                    
+                })}
             </section>
         </main>
     )
