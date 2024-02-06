bd6908e6769145c573b2844fbfb0767c10b5fbe8
Marcel Weber <mrweber@gmx.ch>
2024-02-06 13:15:37 +0100

    Structure more component based

--- app/resultate/page.tsx
+++ app/resultate/page.tsx
@@ -1,7 +1,8 @@
-import Link from "next/link";
-import s from "@/styles/Resultate.module.css"
-import LinkButton from "@/components/LinkButton"
 import { Directory, DirectoryResponse, FileResponse } from "../../interfaces"
+import ChapterTitle from "@/components/ChapterTitle";
+import React from "react";
+import LinkContainer from "@/components/LinkContainer";
+import Archive from "@/components/Archive";
 
 async function getDirs(){
     const date:Date = new Date();
@@ -63,47 +64,30 @@ export default async function Resultate() {
     <main className="main">
       <section className="section">
         <h1>Resultate</h1>
-        <div className={s.introContainer}>
-          <p>
-            Hier sind alle Resultate der auswärtigen Schiessen für das aktuelle
-            Jahr <span>{`${currentYear}`}</span> aufgelistet. Ältere Resultate
-            vergangener Jahre finden sich im Archiv am Ende der Seite. Die
-            Resultate sind nach Aktualität geordnet.
-          </p>
+        <div className="chapter">
+          <div className="chapter_text" dangerouslySetInnerHTML={{__html: 
+            `Hier sind alle Resultate der auswärtigen Schiessen für das aktuelle
+            Jahr ${currentYear} aufgelistet. 
+            Ältere Resultate vergangener Jahre finden sich im Archiv am Ende der Seite. 
+            Die Resultate sind nach Aktualität geordnet.`}}>
+          </div>
         </div>
         {
           sortedYearDirectoryList.length == 0 
           ? 
-          <h2>{`${currentYear} war noch nichts los...`}</h2>
+          <ChapterTitle title={`${currentYear} war noch nichts los...`} />
           :
           sortedYearDirectoryList.map((years:Directory) => {
             const title:string = years.path.split("/")[2]
             return (
-              <div className={s.container} key={`fragment_${years.id}`}>
-                <h2 key={`resultTitle_${title}`}>
-                  {title}
-                </h2>
-                <div key={`linkContainer_${title}`} className={s.linkContainer}>
-                  {fileList.map(fileObject => {
-                    return fileObject.data.map(file=>{
-                      if(file.parent_id == years.id){
-                        return <LinkButton file={file} />
-                      }
-                    });
-                  })}
-                </div>
-              </div>
+              <React.Fragment key={`fragment_${years.id}`}>
+                <ChapterTitle title={title} key={`resultTitle_${title}`} />
+                <LinkContainer fileList={fileList} year={years.id}/>
+              </React.Fragment>
             );
           })
         }
-        <hr />
-        <Link
-          className={s.archiv}
-          href={`https://kdrive.infomaniak.com/app/share/608492/e3b365ba-9347-441c-ac26-ad9a9d6c72c5`}
-          target={"_blank"}
-        >
-          <span className={s.archivText}>Archiv</span>
-        </Link>
+        <Archive />
       </section>
     </main>
   );
