b38706160da0a06b564e1878616cb7d8824d860d
m00gendai <mrweber@gmx.ch>
2024-02-16 14:56:07 +0100

    Ported footer to TS

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,20 +1,20 @@
 import Link from "next/link"
-import s from "../styles/footer.module.css"
+import s from"@/styles/footer.module.css"
 
 export default function Footer(){
 
-    const date = new Date()
-    const currentYear = date.getFullYear()
+    const date:Date = new Date()
+    const currentYear:number = date.getFullYear()
 
     return(
         <footer className={s.footer}>
             <div className={s.desktop}>
                     <p>
-                        ©{currentYear.toString() == "2022" ? "2022" : `2022 - ${currentYear}`} Pistolenclub Hallau
+                        ©{`2022 - ${currentYear}`} Pistolenclub Hallau
                     </p>
                     <div className={s.linkContainer}>
                         <Link className={s.link} href="/impressum">Impressum</Link>
-                        <Link className={s.link} href="/datenschutzerklaerung">Datenschutz</Link>
+                        <Link className={s.link} href="/datenschutz">Datenschutz</Link>
                     </div>
                 </div>
         </footer>
