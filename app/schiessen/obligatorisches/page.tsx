1c233416fd14039e9288e852f71ce2245c3983f0
m00gendai <mrweber@gmx.ch>
2024-02-27 23:46:44 +0100

    Inserted Shooting Days component

--- app/schiessen/obligatorisches/page.tsx
+++ app/schiessen/obligatorisches/page.tsx
@@ -6,6 +6,7 @@ import { innerTextReplacer, pageMetadata } from "@/utils"
 import Gallery from "@/components/Gallery"
 import Spacer from "@/components/Spacer"
 import React from "react"
+import ShootingDays from "@/components/ShootingDays/ShootingDays"
 
 async function getPageContent(){
     const getPageContent:Response = await fetch(
@@ -44,6 +45,8 @@ export default async function Obli(){
                         Daher gibt es auch keine Mindestpunktzahl, um das Obligatorische zu bestehen.`}}>
                     </div>
                 </div>
+                <ChapterTitle title={`Schiessdaten`} />
+                <ShootingDays />
                 {pageContent.map(content=>{
                     return(
                         <React.Fragment key={content._id}>
