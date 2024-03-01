577e525d417169783beb3e5815d198ef98ef90e8
m00gendai <mrweber@gmx.ch>
2024-03-01 19:54:43 +0100

    Added Type and Distnce Icon So you immediately see if its for rifle/pistol and the distance

--- components/ShootingDays/ShootingDayContainer.tsx
+++ components/ShootingDays/ShootingDayContainer.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react"
 import MapLink from "./MapLink"
 import s from "./ShootingDays.module.css"
+import Image from "next/image"
+import { getIcon } from "@/utils"
 
 interface Props{
     shootingDays: SwissgriddedShootingDays
@@ -31,31 +33,43 @@ export default function ShootingDayContainer({shootingDays}:Props){
             <div className={s.container}>
                 {shootingDays.items.map((item, index)=>{
                     if((index+1) > (currentPage*displayPages)-displayPages && (index+1 <= (currentPage*displayPages))){
+                        const icon = getIcon(item.disciplineShortNameGerman)
                     return(
                         <div className={s.entry} key={`entry_${index}`}>
                             <div className={s.dateTime}>
                                 <p className={s.date} dangerouslySetInnerHTML={{__html: new Date(item.from).toLocaleDateString("de-CH", dateOptions)}}></p>
-                                <p className={s.time}>{`${new Date(item.from).toLocaleTimeString("de-CH", timeOptions)} - ${new Date(item.from).toLocaleTimeString("de-CH", timeOptions)}`}</p>
+                                <p className={s.time}>{`${new Date(item.from).toLocaleTimeString("de-CH", timeOptions)} - ${new Date(item.to).toLocaleTimeString("de-CH", timeOptions)}`}</p>
                             </div>
                             <div className={s.info}>
                                 <p className={s.location}>{item.firingRangeName}</p>
                                 <p className={s.club}>{item.organizationName}</p>
+                            </div>
+                            <div className={s.image}>
+                            <Image 
+                                src={getIcon(item.disciplineShortNameGerman)}
+                                fill={true}
+                                style={{objectFit: "contain"}}
+                                alt={"Pistole 50/25m"}
+                            />
                             </div>
                             <MapLink place={item.firingRangeName} coord1={item.coordinates.northing} coord2={item.coordinates.easting} />
                         </div>
                     )}
                 })}
             </div>
+            {shootingDays.totalItems > displayPages ?
             <div className={s.pagination}>
                 {pagesArray.map(page=>{
                     return(
-                        <div className={s.pageButton} key={`page_${page+1}`} onClick={()=>setCurrentPage(page+1)}>
+                        <div className={s.pageButton} key={`page_${page+1}`} onClick={()=>setCurrentPage(page+1)} style={currentPage === page+1 ? {backgroundImage: "linear-gradient(315deg, #e7eff9 0%, #cfd6e6 74%)"}:{}}>
                             {page+1}
                         </div>
                     )
                 })
                 }
             </div>
+            :
+            null}
         </>
     )
 }
