ee8cf13710672b97b67c1cb6912d210347a92b71
m00gendai <mrweber@gmx.ch>
2024-03-03 10:53:11 +0100

    Used next-third-party to conditionally implement google analytics Also implemented my own cookie banner Both are handled conditionally based on cookie existence and values

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,23 @@
+import CookieBanner from "@/components/Cookies/CookieBanner"
 import Navbar from "@/components/Navbar/navbar"
 import Navbar_Mobile from "@/components/Navbar/navbar_mobile"
 import Footer from "@/components/footer"
 import "@/styles/globals.css"
-import CookieProvider from "@/components/Cookies/CookieProvider"
+import { GoogleAnalytics } from "@next/third-parties/google"
+import { cookies } from "next/headers"
 
 export default function RootLayout({children}:{children:React.ReactNode}){
+   const analyticsAllowed:string | undefined = cookies().get("pchallau_analytics")?.value
+
     return(
         <html lang="de">
             <body>
                 <Navbar_Mobile />
                 <Navbar />
-                <CookieProvider>
                 {children}
-                </CookieProvider>
                 <Footer />
+                {analyticsAllowed === undefined ? <CookieBanner /> : null}
+                {analyticsAllowed === "true" ? <GoogleAnalytics gaId={`${process.env.NEXT_PUBLIC_GA}`} /> : null}
             </body>
         </html>
     )
