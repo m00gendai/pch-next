c7b868a21b14627f7f28de8f5892df15be859896
m00gendai <mrweber@gmx.ch>
2024-04-08 11:39:04 +0200

    Offloaded result data fetching to dedicated components This is so the page loads fast and the results are within a suspense boundary. Previously, the page loaded everything on init, which was slow once many results are loaded

--- app/schiessen/schwabenkrieg-erinnerungsschiessen/page.tsx
+++ app/schiessen/schwabenkrieg-erinnerungsschiessen/page.tsx
@@ -1,14 +1,14 @@
-import { PageContent, Table, skesTimes, Document, Medium, DirectoryResponse, Directory, FileResponse } from "@/interfaces";
+import { PageContent, Table, skesTimes, Document, Medium } from "@/interfaces";
 import PlanContainer from "@/components/PlanContainer";
 import ChapterTitle from "@/components/ChapterTitle";
 import { innerTextReplacer, pageMetadata } from "@/utils";
 import TableContainer from "@/components/TableContainer";
 import DocumentContainer from "@/components/DocumentContainer";
 import Gallery from "@/components/Gallery";
-import revalidate from "@/app/actions/revalidate";
-import React from "react";
-import LinkContainer from "@/components/LinkContainer";
+import React, { Suspense } from "react";
 import Spacer from "@/components/Spacer";
+import LoadingSkeleton from "@/components/loadingSkeleton";
+import SkesResultContainer from "@/components/SkesResultContainer";
 
 async function getSkesTimes(){
   const getSkesTimes:Response = await fetch(
@@ -46,61 +46,9 @@ async function getPageContent(){
   return pageContent
 }
 
-async function getDirs(){
-  const date:Date = new Date();
-  const currentYear:number = date.getFullYear();
 
-  // searches all folders with {currentYear} as directory name and includes the directory path (to extract the parent directory name easier)
-  const getYearDirectoryList:Response = await fetch(
-      `https://api.infomaniak.com/2/drive/${process.env.DRIVE}/files/search?with=path&directory_id=${process.env.SKES}&depth=unlimited&types[]=dir&query="${currentYear}"&per_page=1000`,
-      {
-          method: "GET",
-          headers: {
-              Authorization: `Bearer ${process.env.KDRIVE}`,
-              "Content-Type": "application/json",
-          },
-          next: {
-            tags: ["resultDirs"]
-          }
-      }
-  );
-  
-  const yearDirectoryList:DirectoryResponse = await getYearDirectoryList.json();
-if(yearDirectoryList.data.length === 0){
-  return yearDirectoryList.data
-}
-  // sorts the year folders by last_modified_at (latest first)
-  const sortedYearDirectoryList:Directory[] = yearDirectoryList.data.sort((a:Directory, b:Directory) => {
-      const x:number = a.added_at;
-      const y:number = b.added_at;
-      return x < y ? 1 : x > y ? -1 : 0;
-  })
 
-  return sortedYearDirectoryList
-}
 
-async function getFiles(sortedYearDirectoryList:Directory[]){
-  // gets all files within the year directories
-  
-  const fileList:FileResponse[] = await Promise.all(sortedYearDirectoryList.map(async directory =>{
-      const getFiles:Response = await fetch(
-          `https://api.infomaniak.com/2/drive/${process.env.DRIVE}/files/${directory.id}/files`,
-          {
-              method: "GET",
-              headers: {
-                  Authorization: `Bearer ${process.env.KDRIVE}`,
-                  "Content-Type": "application/json",
-              },
-              next: {
-                tags: ["resultFiles"]
-              }
-          }
-      );
-          const files:FileResponse = await getFiles.json();
-          return files
-  }))
-  return fileList
-}
 
 export async function generateMetadata(){
   return pageMetadata("Schwabenkrieg-Erinnerungsschiessen")
@@ -108,13 +56,7 @@ export async function generateMetadata(){
 
 export default async function SKES() {
 
-  revalidate("ResultDirs")
-  revalidate("ResultFiles")
-  
-  const date:Date = new Date();
-  const currentYear:number = date.getFullYear();
 
-  const sortedYearDirectoryList:Directory[] = await getDirs()
   const skesTimes:skesTimes = await getSkesTimes()
   const pageContent:PageContent[] = await getPageContent()
 
@@ -137,21 +79,9 @@ export default async function SKES() {
             fern eine Möglichkeit zum friedlichen Wettkampf bieten.`}}>
           </div>
         </div>
-        {
-          sortedYearDirectoryList.length == 0 
-          ? 
-          null
-          :
-          sortedYearDirectoryList.map(async (years:Directory) => {
-            const fileList:FileResponse[] = await getFiles(sortedYearDirectoryList)
-            return (
-              <React.Fragment key={`fragment_${years.id}`}>
-                <ChapterTitle title={`Resultate ${currentYear}`} />
-                <LinkContainer fileList={fileList} year={years.id}/>
-              </React.Fragment>
-            );
-          })
-        }
+        <Suspense fallback={<LoadingSkeleton />}>
+          <SkesResultContainer />
+        </Suspense>
         
         <ChapterTitle title={skesTimes.title} />
         <div className="chapter">
